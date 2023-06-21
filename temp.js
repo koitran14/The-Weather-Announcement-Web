d9f11eec94e211dac506cd6c422514c4f11a386d
Trần Ngọc Đăng Khôi <khoitranngocdg@gmail.com>
2023-06-21 13:57:53 +0700

    finish getting data from api
    im gonna add some features like bright/dark mode or languages mode

--- temp.js
+++ temp.js
@@ -3,21 +3,8 @@ const search = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
-const body = document.querySelector('body');
-const ui = document.querySelector('#header .logo');
-const icon = document.querySelector('#header .logo i');
-const owner = document.querySelector('#header .owner h5');
 
-
-const searchBox = document.querySelector('.search-box input');
-search.addEventListener('click', fetchWeatherData);
-searchBox.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
-    fetchWeatherData();
-  }
-});
-
-function fetchWeatherData() {
+search.addEventListener('click', () => {
 
     const APIKey = 'ba4398125880514f2dc1c41a3e37ce7d';
     const city = document.querySelector('.search-box input').value;
@@ -28,18 +15,13 @@ function fetchWeatherData() {
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIKey}`)
         .then(response => response.json())
         .then(json => {
-                   
-            if (json.length === 0) {
+
+            if (json.cod === '404') {
                 container.style.height = '400px';
                 weatherBox.style.display = 'none';
                 weatherDetails.style.display = 'none';
                 error404.style.display = 'block';
                 error404.classList.add('fadeIn');
-                body.style.background = '#06283D';
-                ui.style.background = '#326d8599';
-                ui.style.borderColor = 'white';
-                icon.style.color = 'white';
-                owner.style.color = 'white';
                 return;
             }
 
@@ -61,48 +43,25 @@ function fetchWeatherData() {
 
                 switch (json.weather[0].main) {
                     case 'Clear':
-                        image.src = 'res/clear.png';
-                        body.style.background = '#ffd6079b';
-                        ui.style.background = '#ffffffa7';
-                        ui.style.borderColor = '#c35907';
-                        icon.style.color = '#c35907';
-                        owner.style.color = '#c35907';
+                        image.src = 'images/clear.png';
                         break;
 
                     case 'Rain':
-                        image.src = 'res/rain.png';
-                        body.style.background = '#34879B';
-                        ui.style.background = '#ffffffa7';
-                        icon.style.color = '#34879B';
-                        owner.style.color = 'white';
+                        image.src = 'images/rain.png';
                         break;
 
                     case 'Snow':
-                        image.src = 'res/snow.png';
-                        body.style.background = '#30ccf3';
-                        ui.style.background = '#ffffffa7';
-                        ui.style.borderColor = 'white';
-                        icon.style.color = '#30ccf3';
-                        owner.style.color = 'white';
+                        image.src = 'images/snow.png';
                         break;
 
                     case 'Clouds':
-                        image.src = 'res/cloud.png';
-                        body.style.background = '#427290';
-                        ui.style.background = '#ffffffa7';
-                        ui.style.borderColor = '#124666';
-                        icon.style.color = '#124666';
-                        owner.style.color = 'white';
+                        image.src = 'images/cloud.png';
                         break;
 
                     case 'Haze':
-                        image.src = 'res/mist.png';
-                        body.style.background = '#5b6f75';
-                        ui.style.background = '#ffffffa7';
-                        icon.style.color = '#5b6f75';
-                        owner.style.color = 'white';
-                        ui.style.borderColor = '#c35907';
+                        image.src = 'images/mist.png';
                         break;
+
                     default:
                         image.src = '';
                 }
@@ -110,7 +69,7 @@ function fetchWeatherData() {
                 temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
                 description.innerHTML = `${json.weather[0].description}`;
                 humidity.innerHTML = `${json.main.humidity}%`;
-                wind.innerHTML = `${parseInt(json.wind.speed)} Km/h`;
+                wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
 
                 weatherBox.style.display = '';
                 weatherDetails.style.display = '';
@@ -118,5 +77,7 @@ function fetchWeatherData() {
                 weatherDetails.classList.add('fadeIn');
                 container.style.height = '590px';
             })
-        })
-    }
+        });
+
+
+});
